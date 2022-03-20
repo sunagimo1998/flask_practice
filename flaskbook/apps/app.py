2b5c1da151f1b98350bc6f0f5819e7449701696f
sunagimo1998 <asstkm212038@gmail.com>
2022-03-20 17:32:08 +0900

    2章完了

--- flaskbook/apps/app.py
+++ flaskbook/apps/app.py
@@ -1,25 +1,18 @@
-from pathlib import Path
-
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 
+from apps.config import config
+
 csrf = CSRFProtect()
 
 db = SQLAlchemy()
 
-def create_app():
+def create_app(config_key):
     app = Flask(__name__)
-
-    app.config.from_mapping(
-        SECRET_KEY="2AZSMss3p5QPbcY2hBsJ",
-        SQLALCHEMY_DATABASE_URI="sqlite:////" +
-        str(Path(Path(__file__).parent.parent, "local.sqlite")),
-        SQLALCHEMY_TRACK_MODIFICATIONS=False,
-        SQLALCHEMY_ECHO=True,
-        WTF_CSRF_SECRET_KEY="AuwzyszU5sugKN7KZs6f"
-    )
+ 
+    app.config.from_object(config[config_key])
 
     csrf.init_app(app)
     db.init_app(app)
